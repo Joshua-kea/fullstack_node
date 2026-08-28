cfed197c73e3d204b75a6eb1b17515f4f8a15bfc
Joshua-kea <josv0002@stud.kea.dk>
2026-08-28 10:33:34 +0200

    variables_1

--- 01_Miscellaneous/variables_1.js
+++ 01_Miscellaneous/variables_1.js
@@ -2,6 +2,8 @@
 
 // Type Coercion
 
+// Rule: Use strict quality checks: === or !==
+
 // node <filename>
 
 // Rule: Use const whenver possible, otherwise use let
@@ -9,3 +11,48 @@
 // console.log("navn");
 
 const myFavoriteNumber = 213;
+
+// const means that it is constant in the assignment
+
+
+// JavaScript object, fordi JSON ikke kan have kommentarer og det skal være "name": "Amin"
+const person = {
+    // key-value pair
+    name: "Amin"
+};
+
+person.age = 123;
+
+// delete person.name;
+
+console.log(person);
+
+const things = ["mouse"];
+
+things.push("car");
+
+// things.pop("");
+
+// Use comma in console.log 
+// because if we use + (concatenate) we might coerce and change the values
+
+console.log(things[0], things[1]);
+
+
+// Data types in JavaScript
+// String, Boolean, Number, BigInt, null, undefined, Object, Symbol
+
+const greetingOne = "Hello";
+
+const greetingTwo = '"Hello"';
+
+const greetingThree = `"Hello" '${person.name}'
+
+
+linje`;
+
+// Benefit: Multi-line
+
+console.log(greetingOne);
+console.log(greetingTwo);
+console.log(greetingThree);
