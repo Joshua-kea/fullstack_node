5be1921fbebc16d52fc8c46d9e4cb3901a117ef8
Joshua-kea <josv0002@stud.kea.dk>
2026-08-28 09:43:06 +0200

    initial commit

---
+++ 01_Miscellaneous/variables_1.js
@@ -0,0 +1,11 @@
+// Node REPL (Read-evaluate-print loop)
+
+// Type Coercion
+
+// node <filename>
+
+// Rule: Use const whenver possible, otherwise use let
+
+// console.log("navn");
+
+const myFavoriteNumber = 213;
